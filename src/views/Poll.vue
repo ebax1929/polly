e14435b9db7e7640517f3de111d00b62909abe4e
Stina Lindberg <stinalindberg@student-216-35.eduroam.uu.se>
2021-12-06 13:08:46 +0100

    uppdatering

--- src/views/Poll.vue
+++ src/views/Poll.vue
@@ -8,19 +8,44 @@
               v-on:answer="submitAnswer"/>
   </div>
 
-  <div id="headlines">
-    <h2>Choose you character</h2>
-  </div>
+
+    <section id="characters">
+      <div id="headLine">
+        <h2>Choose you character</h2>
+      </div>
+
+      <div class="wrapper">
+<!--        <p id="boat" v-on:click=characterChosen(pngpilen.png)>-->
+
+
+        <div v-on:click='characterChosen("boat")'><img  src="boat.png" ></div>
+
+        <div v-on:click='characterChosen("dog")'><img  src="hunden.png" ></div>
+        <div v-on:click='characterChosen("hat")'><img  src="hatten.png" ></div>
+        <div v-on:click='characterChosen("car")'><img  src="bilen.png" ></div>
+
+
+
+      </div>
+
+
+    </section>
 
 
   <label for="name"></label>
   <input type="text" id="name" v-model="fn" required="required" placeholder="Enter your name">
 
     <p id="enterGame" v-on:click=enterGameButton><img id="picture" src="pngpilen.png" ></p>
+
     <h3>LETS GO</h3>
 
 
   </div>
+  <div v-show="showDisplaySecondPage" id="secondPage">
+    {{fn}}
+
+
+    </div>
 
 
 
@@ -44,7 +69,10 @@ export default {
         a: []
       },
       pollId: "inactive poll",
-      showDisplay:true
+      showDisplay:true,
+      showDisplaySecondPage:false,
+      fn:"",
+      myCharacter:""
     }
 
   },
@@ -60,9 +88,29 @@ export default {
       socket.emit("submitAnswer", {pollId: this.pollId, answer: answer})
     },
     enterGameButton: function(){
-      this.showDisplay=false
+      if (this.fn===""){
+        console.log('insert name')
+      }
+      else
+      {
+        this.showDisplay = false
+        this.showDisplaySecondPage = true;
 
-    }
+      }
+
+    },
+    characterChosen: function(character){
+      var myCharacter=character;
+      console.log(myCharacter);
+
+
+
+    },
+    // displayCharacter: function(){
+    //   if (myCharacter==="dog"){
+    //
+    //   }
+    // }
   }
 }
 </script>
@@ -74,7 +122,24 @@ cursor:pointer;
   height:10em;
 
 }
+.wrapper{
+  display: grid;
+  grid-gap: 5em;
+  grid-template-columns: 10em 10em 10em 10em;
+  position: relative;
+  padding-left: 11em;
 
+
+
+
+}
+.wrapper > div > img{
+  cursor:pointer;
+  height:10em;
+  width: 13em;
+
+
+}
 
 
 #firstpage{
