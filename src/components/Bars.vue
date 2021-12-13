978f4a66c03c2f36d77fc13f3112a055dce34e89
Ebba Axelsson <ebba@MacBook-Pro-2.lan>
2021-12-13 16:16:31 +0100

    13/12

--- src/components/Bars.vue
+++ src/components/Bars.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="wrapper">
   <div class="bar" v-for="(item, key) in data" v-bind:key="key">
-    <div v-bind:style="{height: item + 'px'}">
+    <div v-bind:style="{height: item + '7px'}">
       <span> {{item}} </span>
-    </div>
+      </div>
     <div> 
       {{key}}
     </div>
@@ -24,28 +24,35 @@ export default {
 <style scoped>
 .bar {
   display: inline-block;
-  width: 50px;
+  width: 10em;
   vertical-align: bottom;
+  margin: 6em 1em 0em 1em;
+  text-align: left;
+  width: 10em;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  border-radius: 10px;
+  text-align: left;
+  color: #FFFFFF;
 }
 .bar span {
   position: relative;
   top: -1.2em;
 }
 .bar:nth-child(1) div:nth-child(1) {
-  background-color:red;
+  background-color:#FF4500;
 }
 .bar:nth-child(2) div:nth-child(1) {
-  background-color:blue;
+  background-color:#FFA500;
 }
 .bar:nth-child(3) div:nth-child(1) {
-  background-color:teal;
+  background-color:#FFDAB9;
 }
 .bar:nth-child(4) div:nth-child(1) {
-  background-color:purple;
+  background-color:#FFE4B5;
 }
-.bar:nth-child(5) div:nth-child(1) {
-  background-color:yellow;
-}
+
 
 .wrapper {
   padding:3em;
